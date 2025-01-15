e364675159464bd9da0e9037d27f9eb0347f9890
Akvile <andriejunienea@gmail.com>
2025-01-15 21:29:35 +0200

    Create POM structure
    * Add class - Driver
    * Add class - TestBase
    * Add class - Common

---
+++ src/test/java/lt/vcs/pom/pages/Common.java
@@ -0,0 +1,61 @@
+package lt.vcs.pom.pages;
+
+import lt.vcs.pom.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Common {
+
+    public static void openUrl(String url, int seconds) {      //static, kad nereiktu kurti objektu
+        Driver.initChromeDriver();   // cia instancas
+        Driver.getDriver().get(url);
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));  // lauks viso puslapio uzkrovimo
+    }
+
+    public static void openUrl(String url) {      //static, kad nereiktu kurti objektu
+        Driver.initChromeDriver();   // cia instancas
+        Driver.getDriver().get(url);
+    }
+
+    public static void quitDriver() {
+        Driver.getDriver().quit();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    private static List<WebElement> getElements(By locator) {
+        return Driver.getDriver().findElements(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String text) {
+        getElement(locator).sendKeys(text);
+    }
+
+    public static void clear(By locator) {
+        getElement(locator).clear();
+    }
+
+    public static String getTextFromElement(By locator) {
+        return getElement(locator).getText();
+    }
+
+    public static void clickOnElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static void waitElementLocated(By locator, int seconds) {
+        WebDriverWait webDriverWait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(seconds));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void scrollDown() {
+        scrollDown();
+    }
+}
---
+++ src/test/java/lt/vcs/pom/utils/Driver.java
@@ -0,0 +1,26 @@
+package lt.vcs.pom.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static void initChromeDriver() {
+        WebDriverManager.chromiumdriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.70");
+        options.addArguments("--incognito");
+
+        driver = new ChromeDriver(options);
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
